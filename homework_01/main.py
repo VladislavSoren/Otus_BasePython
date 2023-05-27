16f0b8c07e9b5970edf89763e2b9527335ad0ffc
Vladislav <vlad1998ss@mail.ru>
2023-05-27 18:34:44 +0300

    homework_01 tests is passed

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,7 +4,7 @@
 """
 
 
-def power_numbers():
+def power_numbers(*numbers) -> list:
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
@@ -12,6 +12,8 @@ def power_numbers():
     <<< [1, 4, 25, 49]
     """
 
+    result = [number ** 2 for number in numbers]
+    return result
 
 # filter types
 ODD = "odd"
@@ -19,7 +21,24 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+# Функция проверки числа на простоту
+def is_prime(a: int) -> bool:
+    k = 0
+
+    if a in [0, 1]:
+        result = False
+    else:
+        for divisor in range(2, a // 2+1):
+            if a % divisor == 0:
+                k = k+1
+        if k <= 0:
+            result = True
+        else:
+            result = False
+    return result
+
+
+def filter_numbers(int_numbers_list, filter_type) -> list:
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +49,14 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+
+    if filter_type == ODD:
+        result = list(filter(lambda x: x % 2 != 0, int_numbers_list))
+    elif filter_type == EVEN:
+        result = list(filter(lambda x: x % 2 == 0, int_numbers_list))
+    elif filter_type == PRIME:
+        result = list(filter(is_prime, int_numbers_list))
+    else:
+        result = ['Uknown filter_type']
+
+    return result
