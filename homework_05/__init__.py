1cca8ea0d58d7ca6b0c65812ea5b243a23025633
Vladislav <vlad1998ss@mail.ru>
2023-06-21 13:46:58 +0300

    homework_05: finished

--- homework_05/__init__.py
+++ homework_05/__init__.py
@@ -3,3 +3,11 @@ from . import app
 __all__ = [
     "app",
 ]
+
+# # Путевой кастыль
+# from pathlib import Path
+# import sys
+#
+# BD_PATH = Path(__file__).parent.parent
+# sys.path.append(str(BD_PATH))
+# print(*sys.path, sep="\n")
