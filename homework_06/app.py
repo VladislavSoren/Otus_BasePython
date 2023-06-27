8bf296a27acab5fb06219473e75eab1d20dd6bc7
Vladislav <vlad1998ss@mail.ru>
2023-06-27 10:37:11 +0300

    homework_06: checkpoint6: fixing app config

--- homework_06/app.py
+++ homework_06/app.py
@@ -25,44 +25,11 @@ migrate = Migrate(app=app, db=db)
 # Setting csrf protection
 csrf = CSRFProtect(app)
 
+
 @app.get("/", endpoint="index")
 def hello_root():
     return render_template("index.html")
 
 
-# /hello и /hello/ - два разных адреса!
-# @app.get("/hello/")
-def hello():
-    # http://127.0.0.1:5000/hello/?spam=egs&foo=1&foo=2
-    print(request.args)
-    print(request.args.get("spam"))
-    print(request.args.get("foo"))
-    print(request.args.getlist("foo"))
-
-    name = request.args.get("name")
-    if not name:
-        name = "World"
-    else:
-        name.strip()
-    return f"Hello {name}!"
-
-
-@app.get("/loading/")  # one
-def loading_page():
-    return render_template("loading.html")
-
-
-@app.get("/hello/")  # two
-@app.get("/hello/<name>/")  # one
-def hello_path(name: str | None = None):
-    if name is None:
-        name = request.args.get("name", "")  # если НИЧЕГО не нашли -> ""
-    name = name.strip()
-    if not name:  # альтернатива if name == ""
-        name = "World"
-    return f"Hello {name}!"
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
-    # app.run()
+    app.run(host="0.0.0.0")
