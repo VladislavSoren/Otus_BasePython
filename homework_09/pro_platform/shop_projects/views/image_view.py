8959362b824a041c5cf615ebff5da289e2d1b7d7
Soren <vlad1998ss@mail.ru>
2023-07-12 09:43:03 +0300

    homework_09: image template render only by absolute path 2

--- homework_09/pro_platform/shop_projects/views/image_view.py
+++ homework_09/pro_platform/shop_projects/views/image_view.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
 
 from shop_projects.forms import ImageSexAgeDetectForm
@@ -7,16 +7,34 @@ from shop_projects.forms import ImageSexAgeDetectForm
 # Create your views here.
 
 
-def image_for_predict_view(request):
+def image_for_predict_view(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = ImageSexAgeDetectForm(request.POST, request.FILES)
 
         if form.is_valid():
             form.save()
-            return redirect('success')
+            return redirect('shop_projects:success')
     else:
         form = ImageSexAgeDetectForm()
-    return render(request, '/image_upload.html', {'form': form})
+    return render(
+        request=request,
+        template_name='/shop_projects/image_upload_new.html',
+        context={
+            "form": form,
+        }
+    )
+
+
+# def shop_index(request: HttpRequest) -> HttpResponse:
+#     projects = Project.objects.order_by("id").all()
+#
+#     return render(
+#         request=request,
+#         template_name="shop_projects/index.html",
+#         context={
+#             "projects": projects,
+#         }
+#     )
 
 
 def success(request):
