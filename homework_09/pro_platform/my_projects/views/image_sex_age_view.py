c5b21c6fc3e2d064a0f9266d49fe2e234552aa11
Soren <vlad1998ss@mail.ru>
2023-07-12 17:04:09 +0300

    homework_09: code refactoring

--- homework_09/pro_platform/my_projects/views/image_sex_age_view.py
+++ homework_09/pro_platform/my_projects/views/image_sex_age_view.py
@@ -12,7 +12,7 @@ from asgiref.sync import sync_to_async
 from pro_platform.settings import BASE_DIR
 
 
-async def get_prediction(url: str, data):
+async def get_prediction(url: str, data: dict):
     async with ClientSession() as session:
         async with session.post(url, json=data) as response:
             data: dict = await response.json()
@@ -30,7 +30,11 @@ def image_bytes_to_str(im_path):
 def show_input_image(image_bytes):
     image = Image.open(io.BytesIO(image_bytes))
     image.show()
-    return image
+
+
+def save_tagged_image(image_bytes, path_tagged_image: str):
+    image = Image.open(io.BytesIO(image_bytes))
+    image.save(path_tagged_image)
 
 
 SEX_AGE_HUMANS_DETECTION_SERVICE_URL = " http://127.0.0.1:9988/image"
@@ -43,36 +47,35 @@ async def image_request(request):
             await sync_to_async(form.save)()
 
             # Getting the current instance object to display in the template
-            img_object = form.instance
+            image_object = form.instance
 
-            im_path = img_object.ImgSexAgeDetect.url
-            im_path_abs = BASE_DIR / im_path[1:]  # ignore first "/" in image path
+            # Getting path for saving input image
+            path_input_image = image_object.ImgSexAgeDetect.url
+            path_input_image_abs = BASE_DIR / path_input_image[1:]  # ignore first "/" in image path
 
             # serialization
-            data = {}
-            data['user'] = 'Soren'
-            data['image'] = image_bytes_to_str(im_path_abs)
-            data['image_name'] = os.path.basename(im_path_abs)
+            json_out = {}
+            json_out['user'] = 'Soren'
+            json_out['image'] = image_bytes_to_str(path_input_image_abs)
+            json_out['image_name'] = os.path.basename(path_input_image_abs)
 
             # sending image to service and receiving  response with tagged image
-            json_input: dict = await get_prediction(SEX_AGE_HUMANS_DETECTION_SERVICE_URL, data)
+            json_input: dict = await get_prediction(SEX_AGE_HUMANS_DETECTION_SERVICE_URL, json_out)
 
             #  deserialization
-            image_bytes = base64.b64decode(json_input["tagged_img"])
+            image_bytes = base64.b64decode(json_input["tagged_image"])
 
-            # displaying tagged image
-            image_pillow = show_input_image(image_bytes)
             # absolute path
-            path_tagged_img = BASE_DIR / 'media' / 'images_tagged' / data['image_name']
-            image_pillow.save(path_tagged_img)
+            path_tagged_image = BASE_DIR / 'media' / 'images_tagged' / json_out['image_name']
+            save_tagged_image(image_bytes, path_tagged_image)
 
             # relative path
-            path_tagged_img_for_form = path_tagged_img = f'''/media/images_tagged/{data['image_name']}'''
+            path_tagged_image_for_form = f'''/media/images_tagged/{json_out['image_name']}'''
 
             return render(
                 request,
                 'my_projects/image_sex_age_detect.html',
-                {'form': form, 'img_obj': img_object, 'path_tagged_img': path_tagged_img_for_form}
+                {'form': form, 'image_object': image_object, 'path_tagged_image': path_tagged_image_for_form}
             )
     else:
         form = ImageSexAgeDetectForm()
