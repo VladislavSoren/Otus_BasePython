576352b795bbaa5ab944c24d02e5921d6d66f85e
Soren <vlad1998ss@mail.ru>
2023-07-12 22:00:29 +0300

    homework_09: project work through sync requests lib

--- homework_09/pro_platform/my_projects/views/image_sex_age_view.py
+++ homework_09/pro_platform/my_projects/views/image_sex_age_view.py
@@ -10,6 +10,7 @@ from aiohttp import ClientSession
 from asgiref.sync import sync_to_async
 
 from pro_platform.settings import BASE_DIR
+import requests
 
 
 async def get_prediction(url: str, data: dict):
@@ -17,6 +18,13 @@ async def get_prediction(url: str, data: dict):
         async with session.post(url, json=data) as response:
             data: dict = await response.json()
             return data
+
+
+# make by just request
+def get_prediction_by_req(url: str, json_out: dict):
+    response = requests.post(url, json=json_out)
+    json_input = response.json()
+    return json_input
 
 
 #
@@ -40,11 +48,58 @@ def save_tagged_image(image_bytes, path_tagged_image: str):
 SEX_AGE_HUMANS_DETECTION_SERVICE_URL = " http://127.0.0.1:9988/image"
 
 
+# async def image_request(request):
+#     if request.method == 'POST':
+#         form = ImageSexAgeDetectForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             await sync_to_async(form.save)()
+#
+#             # Getting the current instance object to display in the template
+#             image_object = form.instance
+#
+#             # Getting path for saving input image
+#             path_input_image = image_object.ImgSexAgeDetect.url
+#             path_input_image_abs = BASE_DIR / path_input_image[1:]  # ignore first "/" in image path
+#
+#             # serialization
+#             json_out = {}
+#             json_out['user'] = 'Soren'
+#             json_out['image'] = image_bytes_to_str(path_input_image_abs)
+#             json_out['image_name'] = os.path.basename(path_input_image_abs)
+#
+#             # sending image to service and receiving  response with tagged image
+#             json_input: dict = await get_prediction(SEX_AGE_HUMANS_DETECTION_SERVICE_URL, json_out)
+#
+#             #  deserialization
+#             image_bytes = base64.b64decode(json_input["tagged_image"])
+#
+#             # absolute path
+#             path_tagged_image = BASE_DIR / 'media' / 'images_tagged' / json_out['image_name']
+#             save_tagged_image(image_bytes, path_tagged_image)
+#
+#             # relative path
+#             path_tagged_image_for_form = f'''/media/images_tagged/{json_out['image_name']}'''
+#
+#             return render(
+#                 request,
+#                 'my_projects/image_sex_age_detect.html',
+#                 {'form': form, 'image_object': image_object, 'path_tagged_image': path_tagged_image_for_form}
+#             )
+#     else:
+#         form = ImageSexAgeDetectForm()
+#         # await sync_to_async(form.save)()
+#     return render(
+#         request,
+#         'my_projects/image_sex_age_detect.html',
+#         {'form': form}
+#     )
+
+
 def image_request(request):
     if request.method == 'POST':
         form = ImageSexAgeDetectForm(request.POST, request.FILES)
         if form.is_valid():
-            await sync_to_async(form.save)()
+            form.save()
 
             # Getting the current instance object to display in the template
             image_object = form.instance
@@ -60,7 +115,7 @@ def image_request(request):
             json_out['image_name'] = os.path.basename(path_input_image_abs)
 
             # sending image to service and receiving  response with tagged image
-            json_input: dict = await get_prediction(SEX_AGE_HUMANS_DETECTION_SERVICE_URL, json_out)
+            json_input: dict = get_prediction_by_req(SEX_AGE_HUMANS_DETECTION_SERVICE_URL, json_out)
 
             #  deserialization
             image_bytes = base64.b64decode(json_input["tagged_image"])
